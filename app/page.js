b7ee1bf491d4d6407697e5784616b6b0c43d2126
Estéfane Souza <53922973+estefanecs@users.noreply.github.com>
2023-05-13 10:44:17 -0300

    Update page.js

--- app/page.js
+++ app/page.js
@@ -30,7 +30,7 @@ function SearchBar({ filterText, onFilterTextChange }) {
 function MessageTable({ messages, filterText }) {
   const rows = [];
   messages.forEach((message) => {
-    if (messages[1].includes(filterText)) {
+     if (message[1].toLowerCase().indexOf(filterText.toLowerCase()) === -1) {
       return;
     }
     rows.push(<MessageRow message={message} key={message[1]} />);
